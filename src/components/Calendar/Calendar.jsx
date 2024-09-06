785c9184ae370f5f3995a960528b436602f28bc6
Вячеслав <tfdf63@yandex.ru>
2024-09-06 15:50:33 -0700

    add ticket

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -35,24 +35,48 @@ const Calendar = () => {
 
 	return (
 		<div className='calendar'>
-			{matches.map(match => (
-				<div key={match.id} className='calendar__box'>
-					<div className='calendar__subitem'>
-						<div className='calendar__liga'>{match.liga}</div>
-						<div className='calendar__tour'>1 тур</div>{' '}
-						{/* Можете добавить туры в массив позже */}
-						<div className='calendar__date'>{match.date}</div>
-						<div className='calendar__spectators'>{match.spectators} fans</div>
-					</div>
-					<div className='calendar__item'>
-						<div className='calendar__team_h'>{match.team_h}</div>
-						<div className='calendar__delimiter'>
-							{match.score_h}:{match.score_g}
+			{matches
+				.slice()
+				.reverse()
+				.map(match => (
+					<div
+						key={match.id}
+						className={
+							match.liga === 'РПЛ' ? 'calendar__box' : 'calendar__box_cup'
+						}
+					>
+						<div className='calendar__subitem'>
+							<div className='calendar__liga'>{match.liga}</div>
+							<div className='calendar__tour'>{match.tour} тур</div>{' '}
+							{/* Можете добавить туры в массив позже */}
+							<div className='calendar__date'>{match.date}</div>
+							<div className='calendar__spectators'>
+								{match.spectators !== '' ? `${match.spectators} fans` : ''}
+							</div>
 						</div>
-						<div className='calendar__team_g'>{match.team_g}</div>
+						<div className='calendar__item'>
+							<div className='calendar__team_h'>{match.team_h}</div>
+							<div className='calendar__delimiter'>
+								{match.score_h}:{match.score_g}
+							</div>
+							<div className='calendar__team_g'>{match.team_g}</div>
+						</div>
+						<div className='calendar__ticket'>
+							{match.ticket !== '' ? (
+								<a
+									href={match.ticket}
+									target='_blank'
+									rel='noopener noreferrer'
+									className='calendar__ticket_a'
+								>
+									Купить билет
+								</a>
+							) : (
+								''
+							)}
+						</div>
 					</div>
-				</div>
-			))}
+				))}
 		</div>
 	)
 }
